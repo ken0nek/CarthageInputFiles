e09b6cacce469f8688906b617d9a7ac7b0ceb339
ken0nek <ken.for.develop@gmail.com>
2017-01-02 11:55:57 +0900

    Print additional information and refactor code

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,8 +2,6 @@ import Foundation
 import Commander
 
 // target:
-// prefix: $(SRCROOT)/Carthage/Build/iOS/
-// TODO: - find carthage run script
 
 private let cartfile = "Cartfile"
 private let pbxproj  = "project.pbxproj"
@@ -47,83 +45,106 @@ command(
     let cartfilePath = dir.appendingPath(cartfile)
     let cartfileURL = URL(fileURLWithPath: cartfilePath)
 
-    let pbxprojPath = dir.appendingPath(project).appendingPath(pbxproj)
-    let pbxprojURL = URL(fileURLWithPath: pbxprojPath)
-
     do {
         if try cartfileURL.checkResourceIsReachable() {
             print("Found \(cartfilePath)\n")
         }
+    } catch let error {
+        print("Error: \(error.localizedDescription)")
+        print("Create `Cartfile` and execute `carthage update`")
+        return
+    }
 
-        let prefixWithPlatform = prefix.appendingPath(p)
-        let buildPathWithPlatform = "Carthage/Build".appendingPath(p)
-        guard let enumerator = FileManager.default.enumerator(atPath: buildPathWithPlatform) else { return }
+    let prefixWithPlatform = prefix.appendingPath(p)
+    let buildPathWithPlatform = "Carthage/Build".appendingPath(p)
 
-        var frameworks: [String] = []
-        for f in enumerator {
-            guard let file = f as? String else { return }
-            guard file.hasSuffix(".framework") && file.components(separatedBy: "/").count == 1 else { continue }
-            frameworks.append(file)
-        }
-        print("These frameworks will be added to input files for Carthage Run Scripts\n")
+    print("Searching frameworks in \(buildPathWithPlatform)...\n")
 
-        let frameworksWithPrefix = frameworks.map { prefixWithPlatform.appendingPath($0) }
-        frameworksWithPrefix.forEach { print($0) }
+    guard let enumerator = FileManager.default.enumerator(atPath: buildPathWithPlatform) else { return }
 
-        print("\nis it OK? [[y]/n]")
-        guard let yesno = readLine() else { return }
-        guard yesno == "y" || yesno.isEmpty else { return }
+    var frameworks: [String] = []
+    for f in enumerator {
+        guard let file = f as? String else { return }
 
+        // Only include top level framework
+        guard file.hasSuffix(".framework") && file.components(separatedBy: "/").count == 1 else { continue }
+        frameworks.append(file)
+    }
+
+    guard !frameworks.isEmpty else {
+        print("No frameworks found :(")
+        print("execute `carthage update` or edit `Cartfile`")
+        return
+    }
+
+    print("These frameworks will be added to input files for Carthage Run Scripts\n")
+
+    let frameworksWithPrefix = frameworks.map { prefixWithPlatform.appendingPath($0) }
+    frameworksWithPrefix.forEach { print($0) }
+
+    print("\nis it OK? [[y]/n]")
+    guard let yesno = readLine() else { return }
+    guard yesno == "y" || yesno.isEmpty else { return }
+
+    let pbxprojPath = dir.appendingPath(project).appendingPath(pbxproj)
+    let pbxprojURL = URL(fileURLWithPath: pbxprojPath)
+
+    do {
         if try pbxprojURL.checkResourceIsReachable() {
             print("Found \(pbxprojPath)\n")
         }
+    } catch let error {
+        print("Error: \(error.localizedDescription)")
+        print("Cannot process \(pbxprojPath)")
+        return
+    }
 
-        let data = try Data(contentsOf: pbxprojURL)
+    guard let data = try? Data(contentsOf: pbxprojURL) else { return }
 
-        guard let dic = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any] else { return }
-        var dicToWrite = dic
+    guard let dic = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any] else { return }
+    var dicToWrite = dic
 
-        guard let objects = dic["objects"] as? [String: Any] else { return }
-        var objectsToWrite = objects
+    guard let objects = dic["objects"] as? [String: Any] else { return }
+    var objectsToWrite = objects
 
-        var found = false
-        var key = ""
+    var found = false
+    var key = ""
 
-        for (k, v) in objects {
-            guard let dd = v as? [String: Any] else { continue }
+    for (k, v) in objects {
+        guard let dd = v as? [String: Any] else { continue }
 
-            guard let isa = dd["isa"] as? String else { continue }
-            guard isa == "PBXShellScriptBuildPhase" else { continue }
+        guard let isa = dd["isa"] as? String else { continue }
+        guard isa == "PBXShellScriptBuildPhase" else { continue }
 
-            guard let shellScript = dd["shellScript"] as? String else { continue }
-            guard shellScript.hasSuffix("copy-frameworks") else { continue }
-            guard let inputPaths = dd["inputPaths"] as? [String] else { continue }
-            found = true
-            key = k
-        }
+        guard let shellScript = dd["shellScript"] as? String else { continue }
+        guard shellScript.hasSuffix("copy-frameworks") else { continue }
+        guard let inputPaths = dd["inputPaths"] as? [String] else { continue }
+        found = true
+        key = k
+    }
 
-        if !found {
-            print("This project has no run scripts for Carthage")
-            return
-            //                print("Would you like to add? [[y]/n]")
-            //                guard let yesno = readLine() else { return }
-            //                guard yesno == "y" || yesno.isEmpty else { return }
-            //                print("Not implemented") // cannot set key
-        }
+    if !found {
+        print("This project has no run scripts for Carthage")
+        print("[Xcode] -> [Targets] -> [Build Phases]: [+ New Run Script Phase]")
+        print("and add this command `/usr/local/bin/carthage copy-frameworks`")
+        return
+    }
 
-        var target = objects[key] as! [String: Any]
-        target["inputPaths"] = frameworksWithPrefix
-        objectsToWrite[key] = target
-        dicToWrite["objects"] = objectsToWrite
+    var target = objects[key] as! [String: Any]
+    target["inputPaths"] = frameworksWithPrefix
+    objectsToWrite[key] = target
+    dicToWrite["objects"] = objectsToWrite
 
-        guard let dataToWrite = try? PropertyListSerialization.data(fromPropertyList: dicToWrite, format: .xml, options: .allZeros) else { return }
+    guard let dataToWrite = try? PropertyListSerialization.data(fromPropertyList: dicToWrite, format: .xml, options: .allZeros) else { return }
+
+    do {
         try dataToWrite.write(to: pbxprojURL, options: .atomic)
-
-        print("Finished!")
-
     } catch let error {
         print("Error: \(error.localizedDescription)")
         return
     }
+
+    print("Finished!")
+    print("Please check if everything is fine :)")
 
     }.run()
