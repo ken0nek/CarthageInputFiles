8f555d6bf234914c9c40ac074e9264b35f314a95
ken0nek <ken.for.develop@gmail.com>
2017-01-05 18:45:07 +0900

    Check diff and you can exclude frameworks if any

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "CarthageInputFiles",
     dependencies: [
         .Package(url: "git@github.com:kylef/Commander.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Commander
+import Rainbow
 
 // target:
 
@@ -30,12 +31,12 @@ command(
 ) { project, platform, prefix in
 
     guard (project.hasSuffix("/") && project.hasSuffix("xcodeproj/")) || project.hasSuffix("xcodeproj") else  {
-        print("Please input valid xcode project (*.xcodeproj)")
+        print("Please input valid xcode project (*.xcodeproj)".red)
         return
     }
 
     guard let p = platforms[platform] else {
-        print("Please input valid platform name (ios, mac, watchos, tvos)")
+        print("Please input valid platform name (ios, mac, watchos, tvos)".red)
         return
     }
 
@@ -50,8 +51,57 @@ command(
             print("Found \(cartfilePath)\n")
         }
     } catch let error {
-        print("Error: \(error.localizedDescription)")
+        print("Error: \(error.localizedDescription)".red)
         print("Create `Cartfile` and execute `carthage update`")
+        return
+    }
+
+    let pbxprojPath = dir.appendingPath(project).appendingPath(pbxproj)
+    let pbxprojURL = URL(fileURLWithPath: pbxprojPath)
+
+    do {
+        if try pbxprojURL.checkResourceIsReachable() {
+            print("Found \(pbxprojPath)\n")
+        }
+    } catch let error {
+        print("Error: \(error.localizedDescription)".red)
+        print("Cannot process \(pbxprojPath)")
+        return
+    }
+
+    guard let data = try? Data(contentsOf: pbxprojURL) else { return }
+
+    guard let dic = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any] else { return }
+    var dicToWrite = dic
+
+    guard let objects = dic["objects"] as? [String: Any] else { return }
+    var objectsToWrite = objects
+
+    var found = false
+    var key = ""
+
+    var currentInputPaths: [String] = []
+
+    for (k, v) in objects {
+        guard let dd = v as? [String: Any] else { continue }
+
+        guard let isa = dd["isa"] as? String else { continue }
+        guard isa == "PBXShellScriptBuildPhase" else { continue }
+
+        guard let shellScript = dd["shellScript"] as? String else { continue }
+        guard shellScript.hasSuffix("copy-frameworks") else { continue }
+        guard let inputPaths = dd["inputPaths"] as? [String] else { continue }
+
+        currentInputPaths = inputPaths
+
+        found = true
+        key = k
+    }
+
+    if !found {
+        print("This project has no run scripts for Carthage".red)
+        print("[Xcode] -> [Targets] -> [Build Phases]: [+ New Run Script Phase]")
+        print("and add this command `/usr/local/bin/carthage copy-frameworks`")
         return
     }
 
@@ -72,66 +122,71 @@ command(
     }
 
     guard !frameworks.isEmpty else {
-        print("No frameworks found :(")
+        print("No frameworks found :(".red)
         print("execute `carthage update` or edit `Cartfile`")
         return
     }
 
-    print("These frameworks will be added to input files for Carthage Run Scripts\n")
-
     let frameworksWithPrefix = frameworks.map { prefixWithPlatform.appendingPath($0) }
-    frameworksWithPrefix.forEach { print($0) }
 
-    print("\nis it OK? [[y]/n]")
-    guard let yesno = readLine() else { return }
-    guard yesno == "y" || yesno.isEmpty else { return }
+    let new = Set(frameworksWithPrefix)
+    let current = Set(currentInputPaths)
+    let diff = new.subtracting(current)
 
-    let pbxprojPath = dir.appendingPath(project).appendingPath(pbxproj)
-    let pbxprojURL = URL(fileURLWithPath: pbxprojPath)
+    guard !diff.isEmpty else {
+        print("Nothing to do.\n")
+        print("Current Input Files:\n")
+        currentInputPaths.forEach { print($0) }
+        return
+    }
 
-    do {
-        if try pbxprojURL.checkResourceIsReachable() {
-            print("Found \(pbxprojPath)\n")
+    var frameworksDiff = [String](diff)
+
+    while true {
+
+        print("\nThese frameworks will be newly added to input files for Carthage Run Scripts\n")
+        frameworksDiff.enumerated().forEach { print("\($0): \($1)") }
+
+        print("\nThe result will be:\n")
+        let tmp = currentInputPaths + frameworksDiff
+        tmp.forEach { print($0.green) }
+
+        print("\nis it OK? [[y]/n]")
+        print("Please type the number if there are any frameworks you would like to exclude. (Example: 1, 3)")
+        guard let yesno = readLine() else { return }
+
+        if yesno == "y" || yesno.isEmpty {
+            break
+        } else {
+
+            let exclude = yesno.characters.split(separator: ",").flatMap { Int(String($0).trimmingCharacters(in: .whitespacesAndNewlines)) }
+            let excludeUnique = [Int](Set(exclude))
+
+            guard excludeUnique.count <= frameworksDiff.count else {
+                print("Invalid input: Too many frameworks to exclude".red)
+                return
+            }
+
+            guard let max = excludeUnique.max(), max <= frameworksDiff.count - 1 else {
+                print("Invalid input: Out of bounds".red)
+                return
+            }
+
+            excludeUnique.forEach { frameworksDiff.remove(at: $0) }
+
+            guard !frameworksDiff.isEmpty else {
+                print("Nothing to do.\n")
+                print("Current Input Files:\n")
+                currentInputPaths.forEach { print($0) }
+                return
+            }
         }
-    } catch let error {
-        print("Error: \(error.localizedDescription)")
-        print("Cannot process \(pbxprojPath)")
-        return
     }
 
-    guard let data = try? Data(contentsOf: pbxprojURL) else { return }
-
-    guard let dic = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any] else { return }
-    var dicToWrite = dic
-
-    guard let objects = dic["objects"] as? [String: Any] else { return }
-    var objectsToWrite = objects
-
-    var found = false
-    var key = ""
-
-    for (k, v) in objects {
-        guard let dd = v as? [String: Any] else { continue }
-
-        guard let isa = dd["isa"] as? String else { continue }
-        guard isa == "PBXShellScriptBuildPhase" else { continue }
-
-        guard let shellScript = dd["shellScript"] as? String else { continue }
-        guard shellScript.hasSuffix("copy-frameworks") else { continue }
-        guard let inputPaths = dd["inputPaths"] as? [String] else { continue }
-        found = true
-        key = k
-    }
-
-    if !found {
-        print("This project has no run scripts for Carthage")
-        print("[Xcode] -> [Targets] -> [Build Phases]: [+ New Run Script Phase]")
-        print("and add this command `/usr/local/bin/carthage copy-frameworks`")
-        return
-    }
+    let frameworksToWrite = currentInputPaths + frameworksDiff
 
     var target = objects[key] as! [String: Any]
-    target["inputPaths"] = frameworksWithPrefix
+    target["inputPaths"] = frameworksToWrite
     objectsToWrite[key] = target
     dicToWrite["objects"] = objectsToWrite
 
